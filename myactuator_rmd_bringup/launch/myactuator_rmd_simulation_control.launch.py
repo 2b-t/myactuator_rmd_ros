1e799c60dccc2b256d9deee762c42296f1a9369e
Tobit Flatscher <53856473+2b-t@users.noreply.github.com>
2024-05-27 15:44:54 +0100

    feat: Add flags for launching graphic user interfaces for controllers

--- myactuator_rmd_bringup/launch/myactuator_rmd_simulation_control.launch.py
+++ myactuator_rmd_bringup/launch/myactuator_rmd_simulation_control.launch.py
@@ -10,6 +10,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
@@ -20,8 +21,10 @@ def generate_launch_description():
     actuator = LaunchConfiguration(actuator_parameter_name)
     controller_parameter_name = 'controller'
     controller = LaunchConfiguration(controller_parameter_name)
-    # Add arguments for starting the different GUIs
-    # Add argument for controller file
+    rqt_controller_manager_parameter_name = 'rqt_controller_manager'
+    rqt_controller_manager = LaunchConfiguration(rqt_controller_manager_parameter_name)
+    rqt_joint_trajectory_controller_parameter_name = 'rqt_joint_trajectory_controller'
+    rqt_joint_trajectory_controller = LaunchConfiguration(rqt_joint_trajectory_controller_parameter_name)
 
     actuator_cmd = DeclareLaunchArgument(
         actuator_parameter_name,
@@ -35,8 +38,18 @@ def generate_launch_description():
         default_value='joint_trajectory_controller',
         description='Controller to be spawned'
     )
+    rqt_controller_manager_cmd = DeclareLaunchArgument(
+        rqt_controller_manager_parameter_name,
+        default_value='true',
+        description='Launch rqt controller manager GUI'
+    )
+    rqt_joint_trajectory_controller_cmd = DeclareLaunchArgument(
+        rqt_joint_trajectory_controller_parameter_name,
+        default_value='true',
+        description='Launch rqt joint trajectory controller GUI'
+    )
 
-    description = IncludeLaunchDescription(
+    description_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
                 os.path.join(
@@ -51,18 +64,18 @@ def generate_launch_description():
         }.items()
     )
 
-    joint_state_broadcaster_spawner = Node(
+    joint_state_broadcaster_spawner_node = Node(
         package='controller_manager',
         executable='spawner',
         arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager']
     )
-    controller_spawner = Node(
+    controller_spawner_node = Node(
         package='controller_manager',
         executable='spawner',
         arguments=[controller, '-c', '/controller_manager']
     )
 
-    gazebo = IncludeLaunchDescription(
+    gazebo_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
                 os.path.join(
@@ -72,25 +85,27 @@ def generate_launch_description():
             ]
         )
     )
-    gazebo_spawn_robot = Node(
+    gazebo_spawner_node = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         name='gazebo_spawner',
         arguments=['-entity', actuator, '-topic', 'robot_description'],
-        output="screen"
+        output='screen'
     )
 
-    rqt_controller_manager = Node(
+    rqt_controller_manager_node = Node(
         package='rqt_controller_manager',
         executable='rqt_controller_manager',
         name='rqt_controller_manager',
-        output="screen"
+        output='screen',
+        condition=IfCondition(rqt_controller_manager)
     )
-    rqt_joint_trajectory_controller = Node(
+    rqt_joint_trajectory_controller_node = Node(
         package='rqt_joint_trajectory_controller',
         executable='rqt_joint_trajectory_controller',
         name='rqt_joint_trajectory_controller',
-        output="screen"
+        output='screen',
+        condition=IfCondition(rqt_joint_trajectory_controller)
     )
 
     rviz_file = os.path.join(
@@ -98,7 +113,7 @@ def generate_launch_description():
         'rviz',
         'visualize_myactuator_rmd.rviz'
     )
-    rviz = Node(package='rviz2',
+    rviz_node = Node(package='rviz2',
         executable='rviz2',
         name='rviz2',
         arguments=['--display-config', rviz_file]
@@ -107,12 +122,14 @@ def generate_launch_description():
     ld = LaunchDescription()
     ld.add_action(actuator_cmd)
     ld.add_action(controller_cmd)
-    ld.add_action(description)
-    ld.add_action(joint_state_broadcaster_spawner)
-    ld.add_action(controller_spawner)
-    ld.add_action(gazebo)
-    ld.add_action(gazebo_spawn_robot)
-    ld.add_action(rqt_controller_manager)
-    ld.add_action(rqt_joint_trajectory_controller)
-    ld.add_action(rviz)
+    ld.add_action(rqt_controller_manager_cmd)
+    ld.add_action(rqt_joint_trajectory_controller_cmd)
+    ld.add_action(description_launch)
+    ld.add_action(joint_state_broadcaster_spawner_node)
+    ld.add_action(controller_spawner_node)
+    ld.add_action(gazebo_node)
+    ld.add_action(gazebo_spawner_node)
+    ld.add_action(rqt_controller_manager_node)
+    ld.add_action(rqt_joint_trajectory_controller_node)
+    ld.add_action(rviz_node)
     return ld
